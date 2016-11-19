7ac9741ac74684c7e746e7e40667c5d318fe0f53
Sebas LG <neoranga55@gmail.com>
2016-11-19 01:17:58 +0100

    Enable emu

--- app/src/androidTest/java/com/sebaslogen/blendletje/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sebaslogen/blendletje/ExampleInstrumentedTest.java
@@ -2,12 +2,16 @@ package com.sebaslogen.blendletje;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.sebaslogen.blendletje.ui.activities.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,6 +20,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
