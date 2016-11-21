9fb31fb3e37a6f21ff68a118252d93cbd2674ff5
Sebas LG <neoranga55@gmail.com>
2016-11-21 23:57:31 +0100

    Add recyclerView animation load & prefilled image size

--- app/src/main/java/com/sebaslogen/blendletje/ui/activities/recyclerview/ItemsListAdapter.java
+++ app/src/main/java/com/sebaslogen/blendletje/ui/activities/recyclerview/ItemsListAdapter.java
@@ -1,16 +1,22 @@
 package com.sebaslogen.blendletje.ui.activities.recyclerview;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sebaslogen.blendletje.R;
 import com.sebaslogen.blendletje.domain.model.Article;
 import com.sebaslogen.blendletje.domain.model.ArticleImage;
+import com.sebaslogen.blendletje.domain.model.ImageMetadata;
 import com.sebaslogen.blendletje.domain.model.ListItem;
 import com.squareup.picasso.Picasso;
 
@@ -25,6 +31,8 @@ public class ItemsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     private static final short VIEW_TYPE_ARTICLE = 0;
     private static final short VIEW_TYPE_ADVERTISEMENT = 1;
     private final List<ListItem> mItemsList;
+    private int mLastPosition = -1; // Remember the last item shown on screen for animations
+    private final DecelerateInterpolator mDecelerateInterpolator = new DecelerateInterpolator();
 
     /**
      * Constructor of an adapter for a list of items to bind and display in a recyclerView
@@ -67,6 +75,19 @@ public class ItemsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
             default:
                 throw new IllegalArgumentException("View holder contains an unknown item type");
         }
+        // TODO :Extract common interface with advertisement ViewHolder
+        setAnimation(((ArticleItemViewHolder) holder).getContainer(), position);
+    }
+
+    private void setAnimation(final CardView view, final int position) {
+        if (position > mLastPosition)
+        {
+            final Animation animation = AnimationUtils.loadAnimation(view.getContext(), R.anim.card_appears);
+            animation.setStartOffset(300);
+            animation.setInterpolator(mDecelerateInterpolator);
+            view.startAnimation(animation);
+            mLastPosition = position;
+        }
     }
 
     private void bindArticleItem(final Article article, final ArticleItemViewHolder holder) {
@@ -76,7 +97,8 @@ public class ItemsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
             holder.clearImage();
         } else {
             final ArticleImage articleImage = images.get(0);
-            holder.setImage(articleImage.large().url(), articleImage.caption());
+            final ImageMetadata image = articleImage.large();
+            holder.setImage(image.url(), image.height(), articleImage.caption());
         }
     }
 
@@ -92,23 +114,30 @@ public class ItemsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     private class ArticleItemViewHolder extends RecyclerView.ViewHolder {
 
+        private final CardView mContainer;
         private final TextView mTitle;
         private final ImageView mImage;
         private final String mDefaultImageContentDescription;
+        private final FrameLayout.LayoutParams mDefaultLayoutParams;
 
         ArticleItemViewHolder(final View view) {
             super(view);
+            mContainer = (CardView) view.findViewById(R.id.cv_item_container);
             mTitle = (TextView) view.findViewById(R.id.tv_title);
             mImage = (ImageView) view.findViewById(R.id.iv_image);
             mDefaultImageContentDescription = view.getResources().getString(R.string.article_image_description);
+            mDefaultLayoutParams = new CardView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
         }
 
         void setTitle(final String text) {
             mTitle.setText(getMarkupStrippedString(text));
         }
 
-        void setImage(final String url, final String caption) {
+        void setImage(final String url, final int height, final String caption) {
             mImage.setContentDescription(caption);
+            mImage.setLayoutParams(new CardView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    height));
             final Context context = mImage.getContext();
             Picasso.with(context).cancelRequest(mImage);
             Picasso.with(context)
@@ -120,8 +149,13 @@ public class ItemsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
         void clearImage() {
             Picasso.with(mImage.getContext()).cancelRequest(mImage);
+            mImage.setLayoutParams(mDefaultLayoutParams);
             mImage.setContentDescription(mDefaultImageContentDescription);
             mImage.setImageDrawable(null);
+        }
+
+        CardView getContainer() {
+            return mContainer;
         }
     }
 }
